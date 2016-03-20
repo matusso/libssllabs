7b8d1f7e839c0a3fc9a583d3cb3c2b0f1af8681e
Matúš Bursa <burso@burso.eu>
2016-03-20 22:09:56 +0100

    install libraries, headers..

---
+++ src/ssllabs.cpp
@@ -0,0 +1,9 @@
+//
+// Created by burso on 3/20/16.
+//
+
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
+#include "ssllabs/ssllabs.h"
+
---
+++ src/ssllabs/ssllabs.h
@@ -0,0 +1,18 @@
+//
+// Created by burso on 3/20/16.
+//
+
+#ifndef LIBSSLLABS_SSLLABS_H
+#define LIBSSLLABS_SSLLABS_H
+
+class SSLlabs {
+public:
+    SSLlabs() {};
+    ~SSLlabs() {};
+
+protected:
+
+private:
+};
+
+#endif //LIBSSLLABS_SSLLABS_H
