1b86792903e20fe5043cb9ab1a270187fbd58f34
Matúš Bursa <burso@burso.eu>
2016-03-20 19:40:38 +0100

    prepared enviroment

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+void test() {
+    return;
 }
---
+++ tools/ssltest/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by burso on 3/20/16.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
