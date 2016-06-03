529b111c2911d6e4f880c211b7fd198d54eb883b
Matúš Bursa <burso@burso.eu>
2016-06-03 22:18:28 +0200

    prepare python support for ssllabs

---
+++ python/ssllabs/pyssllabs.cpp
@@ -0,0 +1,23 @@
+//
+// Created by burso on 6/3/16.
+//
+
+#include <python2.7/Python.h>
+#include "../../include/ssllabs/ssllabs.h"
+
+static PyObject* pyssllabs(PyObject* self) {
+    return Py_BuildValue("s", "Hello, Python extensions!!");
+}
+
+static char ssllabs_docs[] =
+        "ssllabs( ): Any message you want to put here!!\n";
+
+static PyMethodDef ssllabs_funcs[] = {
+        {"ssllabs", (PyCFunction)pyssllabs, METH_NOARGS, ssllabs_docs},
+        {NULL}
+};
+
+void init_ssllabs(void) {
+    Py_InitModule3("ssllabs", ssllabs_funcs,
+                   "Extension module example!");
+}
