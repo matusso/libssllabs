efab379311735f73c41aea2be7c2897983bd2cd0
Matúš Bursa <burso@burso.eu>
2016-06-05 19:36:44 +0200

    first python method 'getInfo' implemented

--- python/ssllabs/pyssllabs.cpp
+++ python/ssllabs/pyssllabs.cpp
@@ -4,19 +4,27 @@
 
 #include "../../include/ssllabs/ssllabs.h"
 #include <python2.7/Python.h>
+#include <iostream>
 
-static PyObject *pyssllabs(PyObject *self) {
-    return Py_BuildValue("s", "Hello, Python extensions!!");
+extern "C" {
+static PyObject *getInfo(PyObject *self) {
+    std::string data;
+    ssllabs::SSLlabs sslabs;
+
+    sslabs.getInfo(data);
+    return Py_BuildValue("s", data.c_str());
 }
 
 static char ssllabs_docs[] =
         "ssllabs( ): Any message you want to put here!!\n";
 
 static PyMethodDef ssllabs_funcs[] = {
-        {"ssllabs", (PyCFunction) pyssllabs, METH_NOARGS, ssllabs_docs},
-        {NULL}
+        {"getInfo", (PyCFunction) getInfo, METH_NOARGS, ssllabs_docs},
+        {NULL, NULL, 0, NULL}
 };
 
-void init_ssllabs(void) {
-    Py_InitModule3("ssllabs", ssllabs_funcs, "Extension module example!");
+void initssllabs(void) {
+    Py_InitModule3("ssllabs", ssllabs_funcs,
+                   "Extension module example!");
+}
 }
