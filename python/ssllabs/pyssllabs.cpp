6316a343093ba2c26f1d1ccbe91269ce2f82393c
Matúš Bursa <burso@burso.eu>
2016-06-08 17:52:32 +0200

    trying to fix build on OS X

--- python/ssllabs/pyssllabs.cpp
+++ python/ssllabs/pyssllabs.cpp
@@ -4,27 +4,28 @@
 
 #include "../../include/ssllabs/ssllabs.h"
 #include <python2.7/Python.h>
-#include <iostream>
 
 extern "C" {
-static PyObject *getInfo(PyObject *self) {
-    std::string data;
-    ssllabs::SSLlabs sslabs;
+    #include <iostream>
 
-    sslabs.getInfo(data);
-    return Py_BuildValue("s", data.c_str());
-}
+    static PyObject *getInfo(PyObject *self) {
+        std::string data;
+        ssllabs::SSLlabs sslabs;
 
-static char ssllabs_docs[] =
-        "ssllabs( ): Any message you want to put here!!\n";
+        sslabs.getInfo(data);
+        return Py_BuildValue("s", data.c_str());
+    }
 
-static PyMethodDef ssllabs_funcs[] = {
-        {"getInfo", (PyCFunction) getInfo, METH_NOARGS, ssllabs_docs},
-        {NULL, NULL, 0, NULL}
-};
+    static char ssllabs_docs[] =
+            "ssllabs( ): Any message you want to put here!!\n";
 
-void initssllabs(void) {
-    Py_InitModule3("ssllabs", ssllabs_funcs,
-                   "Extension module example!");
-}
+    static PyMethodDef ssllabs_funcs[] = {
+            {"getInfo", (PyCFunction) getInfo, METH_NOARGS, ssllabs_docs},
+            {NULL, NULL, 0, NULL}
+    };
+
+    void initssllabs(void) {
+        Py_InitModule3("ssllabs", ssllabs_funcs,
+                       "Extension module example!");
+    }
 }
