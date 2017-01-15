f1741c8cfd126f284764294db8f11b21ddfd7b7a
Matúš Bursa <burso@burso.eu>
2017-01-15 12:35:04 +0100

    refactor ssltest tool

--- tools/ssllabs/ssllabs.cpp
+++ tools/ssllabs/ssllabs.cpp
@@ -3,50 +3,48 @@
 #include "../../include/ssllabs/ssllabs.h"
 
 using namespace ssllabs;
-using namespace std;
 
 int main(int argc, char** argv) {
     ssllabs::SSLlabs ssllabsObject;
-    string data;
+    std::string data;
     labsReport_t report;
 
     ssllabsObject.analyze("www.google.com", report);
-    cout << "HOST: " << report.Host << "\n";
-    cout << "PORT: " << report.Port << "\n";
-    cout << "PROTOCOL: " << report.Protocol << "\n";
-    cout << "isPublic: " << report.IsPublic << "\n";
-    cout << "STATUS: " << report.Status << "\n";
-    cout << "STARTTIME: " << report.StartTime << "\n";
-    cout << "TESTTIME: " << report.TestTime << "\n";
-    cout << "ENGINE VERSION: " << report.EngineVersion << "\n";
-    cout << "CRITERIA VERSION: " << report.CriteriaVersion << "\n";
-    vector<labsEndpoint_t>::iterator it;
+    std::cout << "HOST: " << report.Host << "\n";
+    std::cout << "PORT: " << report.Port << "\n";
+    std::cout << "PROTOCOL: " << report.Protocol << "\n";
+    std::cout << "isPublic: " << report.IsPublic << "\n";
+    std::cout << "STATUS: " << report.Status << "\n";
+    std::cout << "STARTTIME: " << report.StartTime << "\n";
+    std::cout << "TESTTIME: " << report.TestTime << "\n";
+    std::cout << "ENGINE VERSION: " << report.EngineVersion << "\n";
+    std::cout << "CRITERIA VERSION: " << report.CriteriaVersion << "\n";
 
+    std::vector<labsEndpoint_t>::iterator it;
     int counter = 1;
     for (it = report.Endpoints.begin(); it != report.Endpoints.end(); it++) {
-        cout << "ENDPOINT " << counter << "\n";
-        labsEndpoint_t endpoint = *it;
-        labsEndpoint_t endpoint2;
-        ssllabsObject.getEndpointData("www.google.com", endpoint.IpAddress, endpoint2);
-        cout << "\tIP ADDRESS: " << endpoint2.IpAddress << "\n";
-        cout << "\tSERVER NAME: " << endpoint2.ServerName << "\n";
-        cout << "\tSTATUS MESSAGE: " << endpoint2.StatusMessage << "\n";
-        cout << "\tGRADE: " << endpoint2.Grade << "\n";
-        cout << "\tGRADE TRUST IGNORED: " << endpoint2.GradeTrustIgnored << "\n";
-        cout << "\tHAS WARNINGS: " << endpoint2.HasWarnings << "\n";
-        cout << "\tISEXCEPTIONAL: " << endpoint2.IsExceptional << "\n";
-        cout << "\tPROGRESS: " << endpoint2.Progress << "\n";
-        cout << "\tDURATION: " << endpoint2.Duration << "\n";
-        cout << "\tETA: " << endpoint2.Eta << "\n";
-        cout << "\tDELEGATION: " << endpoint2.Delegation << "\n";
+        std::cout << "ENDPOINT " << counter << "\n";
+        labsEndpoint_t endpoint;
+        ssllabsObject.getEndpointData("www.google.com", it->IpAddress, endpoint);
+        std::cout << "\tIP ADDRESS: " << endpoint.IpAddress << "\n";
+        std::cout << "\tSERVER NAME: " << endpoint.ServerName << "\n";
+        std::cout << "\tSTATUS MESSAGE: " << endpoint.StatusMessage << "\n";
+        std::cout << "\tGRADE: " << endpoint.Grade << "\n";
+        std::cout << "\tGRADE TRUST IGNORED: " << endpoint.GradeTrustIgnored << "\n";
+        std::cout << "\tHAS WARNINGS: " << endpoint.HasWarnings << "\n";
+        std::cout << "\tISEXCEPTIONAL: " << endpoint.IsExceptional << "\n";
+        std::cout << "\tPROGRESS: " << endpoint.Progress << "\n";
+        std::cout << "\tDURATION: " << endpoint.Duration << "\n";
+        std::cout << "\tETA: " << endpoint.Eta << "\n";
+        std::cout << "\tDELEGATION: " << endpoint.Delegation << "\n";
 
-        cout << "\t\tHOST START TIME: " << endpoint2.Details.HostStartTime << "\n";
-        cout << "\t\tKEY INFORMATION\n";
-        cout << "\t\t\tSIZE: " << endpoint2.Details.Key.Size << "\n";
-        cout << "\t\t\tALGORTIHM: " << endpoint2.Details.Key.Algorithm << "\n";
-        cout << "\t\t\tDEBIAN FLAW: " << endpoint2.Details.Key.DebianFlaw << "\n";
-        cout << "\t\t\tSTRENGTH: " << endpoint2.Details.Key.Strength << "\n";
-        cout << "\t\t\tQ: " << endpoint2.Details.Key.Q << "\n";
+        std::cout << "\t\tHOST START TIME: " << endpoint.Details.HostStartTime << "\n";
+        std::cout << "\t\tKEY INFORMATION\n";
+        std::cout << "\t\t\tSIZE: " << endpoint.Details.Key.Size << "\n";
+        std::cout << "\t\t\tALGORTIHM: " << endpoint.Details.Key.Algorithm << "\n";
+        std::cout << "\t\t\tDEBIAN FLAW: " << endpoint.Details.Key.DebianFlaw << "\n";
+        std::cout << "\t\t\tSTRENGTH: " << endpoint.Details.Key.Strength << "\n";
+        std::cout << "\t\t\tQ: " << endpoint.Details.Key.Q << "\n";
         counter++;
     }
     return 0;
